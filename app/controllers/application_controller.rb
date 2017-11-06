7d57d43cca0bb6cabb7aba978b91f51b9af0d1c2
divyanshumehta <divyanshumehta@outlook.com>
2017-11-06 18:21:34 +0530

    Removed complete fields check for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
              reset_password_sent_at remember_created_at sign_in_count u_id placed_at c_at
              current_sign_in_at last_sign_in_at current_sign_in_ip last_sign_in_ip}
       fields.each do |field|
-        if current_user[field].blank?
+        if current_user[field].blank? and current_user.roles_mask != 1
           flash[:notice] = "Fill this form to proceed"
           redirect_to force_update_url
           break
